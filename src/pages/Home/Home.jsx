34d1b0a1f58494627b0dffbd0a8efbbc65596353
verena_mories <98613146+VerenaMories@users.noreply.github.com>
2022-08-23 15:29:15 +0200

    a

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../Home/Home.css'
+export default function Home() {
+
+  return (
+  <>
+    <div className="container">
+        <div className="row">
+        <h2>Meet Digitail, the</h2> <div class="indent1">
+
+<h1 className="animated-text">Produktvergleich</h1>
+</div> <h2>veterinary software</h2>
+ <h2>You'd Wish You Had Sooner</h2>
+        </div>
+ 
+    </div>
+ 
+
+
+
+
+  </>
+  )
+}
